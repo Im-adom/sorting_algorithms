c3cdcaca809bd6290cfb46745977588ed66f1d12
Freda Tsorme-Dzebu <fredatd2016@gmail.com>
2023-08-25 09:10:48 -0700

    modified

--- print_list.c
+++ print_list.c
@@ -1,55 +1,23 @@
-#ifndef DECK_H
-#define DECK_H
-
 #include <stdio.h>
+#include "sort.h"
 
 /**
- * enum kind_e - suit enumeration
+ * print_list - Prints a list of integers
  *
- *
- * @SPADE: 0
- * @HEART: 1
- * @CLUB: 2
- * @DIAMOND: 3
+ * @list: The list to be printed
  */
-typedef enum kind_e
+void print_list(const listint_t *list)
 {
-    SPADE = 0,
-    HEART,
-    CLUB,
-    DIAMOND
-} kind_t;
+    int i;
 
-/**
- * struct card_s - Playing card
- *
- * @value: Value of the card
- * From "Ace" to "King"
- * @kind: Kind of the card
- */
-typedef struct card_s
-{
-    const char *value;
-    const kind_t kind;
-} card_t;
-
-
-/**
- * struct deck_node_s - Deck of card
- *
- * @card: Pointer to the card of the node
- * @prev: Pointer to the previous node of the list
- * @next: Pointer to the next node of the list
- */
-typedef struct deck_node_s
-{
-	const card_t *card;
-	struct deck_node_s *prev;
-	struct deck_node_s *next;
-} deck_node_t;
-
-void sort_deck(deck_node_t **deck);
-int lessthan(const card_t *c, const card_t *d);
-void cocktail_sort_list(deck_node_t **list);
-
-#endif /* DECK_H */
+    i = 0;
+    while (list)
+    {
+        if (i > 0)
+            printf(", ");
+        printf("%d", list->n);
+        ++i;
+        list = list->next;
+    }
+    printf("\n");
+}
