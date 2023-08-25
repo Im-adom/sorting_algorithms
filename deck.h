aae4cfaf6f2e1e5b6dd83619506ae4a69bd729cc
Freda Tsorme-Dzebu <fredatd2016@gmail.com>
2023-08-25 08:49:58 -0700

    header file task 12

---
+++ deck.h
@@ -0,0 +1,55 @@
+#ifndef DECK_H
+#define DECK_H
+
+#include <stdio.h>
+
+/**
+ * enum kind_e - suit enumeration
+ *
+ *
+ * @SPADE: 0
+ * @HEART: 1
+ * @CLUB: 2
+ * @DIAMOND: 3
+ */
+typedef enum kind_e
+{
+    SPADE = 0,
+    HEART,
+    CLUB,
+    DIAMOND
+} kind_t;
+
+/**
+ * struct card_s - Playing card
+ *
+ * @value: Value of the card
+ * From "Ace" to "King"
+ * @kind: Kind of the card
+ */
+typedef struct card_s
+{
+    const char *value;
+    const kind_t kind;
+} card_t;
+
+
+/**
+ * struct deck_node_s - Deck of card
+ *
+ * @card: Pointer to the card of the node
+ * @prev: Pointer to the previous node of the list
+ * @next: Pointer to the next node of the list
+ */
+typedef struct deck_node_s
+{
+	const card_t *card;
+	struct deck_node_s *prev;
+	struct deck_node_s *next;
+} deck_node_t;
+
+void sort_deck(deck_node_t **deck);
+int lessthan(const card_t *c, const card_t *d);
+void cocktail_sort_list(deck_node_t **list);
+
+#endif /* DECK_H */
